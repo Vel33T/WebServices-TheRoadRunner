54cb195a88ca8cff07c40b3a378b295eca5e65a4
Vladimir Georgiev <vggeorgiev@mail.bg>
2013-08-14 23:14:11 +0300

    Fix base url!

--- JustRunnerChat/JustRunnerChat/ChatScripts/main.js
+++ JustRunnerChat/JustRunnerChat/ChatScripts/main.js
@@ -1,7 +1,7 @@
 ﻿/// <reference path="controller.js" />
 /// <reference path="dataAccess.js" />
 (function () {
-    var serviceRoot = "http://localhost:16502/api/";
+    var serviceRoot = "http://roadrunnerchat.apphb.com/api/";
     
     var persister = Chat.persisters.get(serviceRoot);
 
